a0e13ab52344a074ef50837fe7ca2d5202f06578
Mark Koudritsky <kamrik@gmail.com>
2024-03-17 22:11:56 -0400

    Try Azure OpenAI

--- client.py
+++ client.py
@@ -1,8 +1,13 @@
 from . import env
-from openai import AsyncOpenAI
+from openai import AsyncOpenAI, AsyncAzureOpenAI
 from .threads_factory import threads_factory
 from .assistants_factory import assistants_factory
 
 client = AsyncOpenAI(api_key=env.API_KEY, organization=env.ORG_ID)
+# client = AsyncAzureOpenAI(
+#     api_key=env.API_KEY, 
+#     azure_endpoint=env.AZURE_ENDPOINT,
+#     api_version='2024-03-01-preview',
+#     )
 get_thread = threads_factory(client)
 get_assistant, asst_filter = assistants_factory(client)
--- env.py
+++ env.py
@@ -1,7 +1,14 @@
+import os
 from os import getenv
+import dotenv
 
+file_dir = os.path.dirname(os.path.abspath(__file__))
+
+dotenv.load_dotenv(dotenv_path= os.path.join(file_dir, ".env"))
 # envirionments (or define here)
-LOG_LEVEL = getenv("LOG_LEVEL", "INFO").upper()
+LOG_LEVEL = getenv("LOG_LEVEL", "DEBUG").upper()
 BOT_TOKEN = getenv("BOT_TOKEN")
 ORG_ID = getenv("ORG_ID")
 API_KEY = getenv("API_KEY")
+AZURE_ENDPOINT = getenv("AZURE_ENDPOINT")
+API_PROVIDER = getenv("API_PROVIDER", "openai")
